20cf4e03096e70fac4438a208fe805cc45ef2ed2
veris-neerajdhiman <neeraj.dhiman@veris.in>
2017-03-09 16:30:35 +0530

    refactor : project structure :hammer: project structure setup neccessary chnage sin settings

--- notifyAll/config/settings.py
+++ notifyAll/config/settings.py
@@ -29,12 +29,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Django core apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # library apps
+    'notifyAll',
 ]
 
 MIDDLEWARE = [
