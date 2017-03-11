5eefca29cf2e12483de140c78657c5189490b7e8
inforian <ndhiman08@gmail.com>
2017-03-11 16:04:23 +0530

    Add : library added to pypi :hammer: :construction_worker:

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name='django-notifyAll',
+  packages=['notifyAll'],
+  version='0.1',
+  description='A lib which you can use for all types of notifications like SMS, Mail, Push.',
+  author='Neeraj Dhiman',
+  author_email='ndhiman08@gmail.com',
+  license='GPL',
+  url='https://github.com/inforian/django-notifyAll',
+  download_url='https://github.com/inforian/django-notifyAll/archive/0.1.tar.gz',
+  keywords=['notification', 'notify', 'sms', 'email', 'push'],
+  classifiers=[],
+)
