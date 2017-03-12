8aee07bc93ee1764423c58fe4c1b0dbcb08b4f06
inforian <ndhiman08@gmail.com>
2017-03-13 00:51:10 +0530

    fix : pypi pkg upload :bug:

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from distutils.core import setup
+from setuptools import find_packages
+
 setup(
   name='django-notifyAll',
-  packages=['notifyAll'],
-  version='0.1',
+  packages=find_packages(exclude=['example']),
+  version='0.1.2',
   description='A lib which you can use for all types of notifications like SMS, Mail, Push.',
   author='Neeraj Dhiman',
   author_email='ndhiman08@gmail.com',
   license='GPL',
   url='https://github.com/inforian/django-notifyAll',
-  download_url='https://github.com/inforian/django-notifyAll/archive/0.1.tar.gz',
+  download_url='https://github.com/inforian/django-notifyAll/archive/0.1.2tar.gz',
   keywords=['notification', 'notify', 'sms', 'email', 'push'],
   classifiers=[],
 )
