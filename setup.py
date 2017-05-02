b6471d64ca81882a6fcae83dd7b2b71c149a04e2
inforian <ndhiman08@gmail.com>
2017-05-02 16:09:29 +0530

    Add : dependency packages added :hammer:

--- setup.py
+++ setup.py
@@ -4,13 +4,16 @@ from setuptools import find_packages
 setup(
   name='django-notifyAll',
   packages=find_packages(exclude=['example']),
-  version='0.2',
+  version='0.2.1',
   description='A lib which you can use for all types of notifications like SMS, Mail, Push.',
   author='Neeraj Dhiman',
   author_email='ndhiman08@gmail.com',
   license='GPL',
   url='https://github.com/inforian/django-notifyAll',
-  download_url='https://github.com/inforian/django-notifyAll/archive/0.2tar.gz',
+  download_url='https://github.com/inforian/django-notifyAll/archive/0.2.1tar.gz',
   keywords=['notification', 'notify', 'sms', 'email', 'push'],
   classifiers=[],
+  install_requires=[
+    'Django>=1.8',
+]
 )
