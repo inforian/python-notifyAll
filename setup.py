eafdbd600fda9c0a785e6b78d09e4c5a27fbabd9
neeraj <ndhiman08@gmail.com>
2017-08-24 18:20:44 +0530

    Update : pypi Release update :construction_worker:

--- setup.py
+++ setup.py
@@ -2,18 +2,15 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-  name='django-notifyAll',
+  name='python-notifyAll',
   packages=find_packages(exclude=['example']),
-  version='0.3',
-  description='A lib which you can use for all types of notifications like SMS, Mail, Push.',
+  version='0.4',
+  description='A library which you can use for all types of notifications like SMS, Mail, Push.',
   author='Neeraj Dhiman',
   author_email='ndhiman08@gmail.com',
   license='GPL',
-  url='https://github.com/inforian/django-notifyAll',
-  download_url='https://github.com/inforian/django-notifyAll/archive/v0.3.tar.gz',
+  url='https://github.com/inforian/python-notifyAll',
+  download_url='https://github.com/inforian/python-notifyAll/archive/v0.4.tar.gz',
   keywords=['notification', 'notify', 'sms', 'email', 'push'],
   classifiers=[],
-  install_requires=[
-    'Django>=1.8',
-]
 )
