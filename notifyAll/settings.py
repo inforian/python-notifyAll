a4df4af1ae8e8a58b6af3ae26e467eb006af95ab
neeraj <ndhiman08@gmail.com>
2017-08-24 16:27:22 +0530

    Add : Plivo & Twilio settings :hammer:

--- notifyAll/settings.py
+++ notifyAll/settings.py
@@ -27,8 +27,10 @@ GMAIL_PASSWORD = os.environ.get('GMAIL_PASSWORD', None)
 SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY', None)
 
 
-#
-# PLIVO_AUTH_ID = None
-# PLIVO_AUTH_TOKEN = None
+# # ------------ Plivo -----------
+PLIVO_AUTH_ID = os.environ.get('PLIVO_AUTH_ID', None)
+PLIVO_AUTH_TOKEN = os.environ.get('PLIVO_AUTH_TOKEN', None)
 
-
+# # ------------ Twilio -----------
+TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID', None)
+TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN', None)
